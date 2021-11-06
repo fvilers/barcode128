10e9295478215bdb4e9fef870838653547761530
Fabian Vilers <fabian.vilers@dev-one.com>
2021-11-06 11:05:50 +0100

    Clean up

--- src/App.tsx
+++ src/App.tsx
@@ -3,18 +3,23 @@ import Barcode from "./Barcode";
 import encodeBarcode from "./barcode-encoder";
 import BarcodeForm from "./BarcodeForm";
 
+type State = {
+  busy: boolean;
+  result?: string;
+};
+
 function App() {
-  const [result, setResult] = useState("");
+  const [{ busy, result }, setState] = useState<State>({ busy: false });
   const handleGeneration = (text: string) => {
-    const encoded = encodeBarcode(text);
-    console.log("Generate barcode with", text, encoded);
-    setResult(encoded);
+    // Seems stupid right now but we'll move the barcode generation to an API later on
+    setState({ busy: true, result: undefined });
+    setState({ busy: false, result: encodeBarcode(text) });
   };
 
   return (
     <main>
       <h1>Barcode 128</h1>
-      <BarcodeForm onSubmit={handleGeneration} />
+      <BarcodeForm disabled={busy} onSubmit={handleGeneration} />
 
       {result && <Barcode text={result} />}
     </main>
